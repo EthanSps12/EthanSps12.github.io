28b00a780ebbe86d1000314ada1775d7572bd457
Ethan Dahlby <78369675+EthanSps12@users.noreply.github.com>
2025-12-15 12:53:03 -0500

    Create ListNode.cpp

---
+++ C++ Postfix Calculator/ListNode.cpp
@@ -0,0 +1,9 @@
+//Ethan Dahlby, ed6tf, 3.4.2021, ListNode.cpp
+#include "ListNode.h"
+using namespace std;
+
+ListNode::ListNode(){  // Default construction
+  next = NULL;
+  previous = NULL;
+  value = 99989;  // Dummy value
+}
